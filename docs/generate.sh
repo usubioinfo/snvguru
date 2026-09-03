d1b629c2d428bb38ae27b4fe21eac18523814741
David Guevara <davguev@gmail.com>
2026-09-02 23:48:18 -0600

    Fix doc generation

--- docs/generate.sh
+++ docs/generate.sh
@@ -3,7 +3,7 @@ set -e
 rm -rf _build/html 
 sphinx-build . ./_build/html -a 
 
-for name in hcapsulatum mtuberculosis influenzaA influenzaA3; do
+for name in hcapsulatum mtuberculosis influenzaA; do
     if [ -d "$HOME/pipeline/$name" ]; then
         ln -snf "$HOME/pipeline/$name" "./_build/html/$name"
     elif [ -d "$HOME/pipeline/test/$name" ]; then
